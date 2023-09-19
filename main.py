610fee05d174c8498cc9dfaee7161979e312dcdb
Matis Bodaghi <matisbodaghi@gmail.com>
2023-09-19 11:05:44 +0100

    Improved robot in main.py by allowing robot to also have a direction, initialised by the user. The robot will move one step forward in the direction it is facing.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 grid_size = 10
 
+# Input robot name and coordinates
 name = input("Robot name: ")
 while True:
     try:
@@ -8,8 +9,8 @@ while True:
     except:
         print("Enter an integer")
         continue
-#"Clipping" the values so the robot is in the grid
 
+# "Clipping" the values so the robot is in the grid
 if row<0: 
     row = 0
 elif row>=grid_size:
@@ -27,10 +28,38 @@ if column<0:
 elif column>=grid_size:
     column = grid_size-1
 
+
+direction = None
+while direction not in ['n', 's', 'e', 'w']:
+    direction = input("Enter a direction ('n', 's', 'e' or 'w')")
+if direction == 'n':
+    row-=1
+    direction_name = "North"
+elif direction == 's':
+    row +=1
+    direction_name = "South"
+elif direction == 'e':
+    column +=1
+    direction_name = "East"
+elif direction == 'w':
+    column -=1
+    direction_name = "West"
+#Clip values again
+if column<0:
+    column = 0
+elif column>=grid_size:
+    column = grid_size-1
+if row<0: 
+    row = 0
+elif row>=grid_size:
+    row = grid_size-1
+
 age = 4
 id = 1
+
 message = f'My name is {name} and my id is {id}.'
 print(message)
+#Find quadrant
 if row<grid_size//2:
     row_half = "top"
 else:
@@ -39,8 +68,12 @@ else:
 if column<grid_size//2:
     column_half = "left"
 else:
-    row_half = "right"
+    column_half = "right"
 
 quadrant = row_half+" "+column_half
-print(f"Current location: ({row},{column}), {quadrant} quadrant")
+print(f"Current location: ({row},{column}), {quadrant} quadrant. I am facing {direction_name}")
+
+
+
+
 
