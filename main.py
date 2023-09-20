7fb35ec0a9dd9ac45354341701538bee82d68fce
Matis Bodaghi <matisbodaghi@gmail.com>
2023-09-20 10:26:26 +0100

    Refactoring the robot. Added functions for clarity and made robot move until stopped

--- main.py
+++ main.py
@@ -1,56 +1,82 @@
 import random
 
-grid_size = 10
+
+def input_direction():
+    direction = None
+    while direction not in ['n', 's', 'e', 'w']:
+        direction = input("Enter a direction ('n', 's', 'e' or 'w'): ").lower()
+    return direction
+
+def generate_position():
+    row = random.randint(0,grid_size-1)
+    column = random.randint(0,grid_size-1)
+    return row,column
+
+
+def move_a_square(row, column, direction):
+    if direction == 'n':
+        row = max(0, row-1)
+    elif direction == 's':
+        row = min(grid_size-1, row+1)
+    elif direction == 'e':
+        column = min(grid_size-1, column+1)    
+    elif direction == 'w':
+        column = max(0, column-1)
+    
+    return row, column
+
+
+def is_stuck(row, column, direction):
+    return (row, column) == move_a_square(row, column, direction)
+
+
+def turn_right(direction):
+    list_of_directions = ['n', 'e', 's', 'w']
+    for d in range(4):
+        if list_of_directions[d] == direction:
+            return(list_of_directions[(d+1) % 4])
+    print('Turning right')
+    
+
+def find_direction_name(direction):
+    return {'n': 'North', 'e': 'East', 's': 'South', 'w': 'West'}[direction]
+
+
+def send_message(row, column, direction, grid_size):
+    direction_name = find_direction_name(direction)
+    print(f"Current location: ({row},{column}). I am facing {direction_name}")
+
+
 
 # Randomise robot name and coordinates
+grid_size = 10
 name = input("Robot name: ")
-row = random.randint(0,grid_size-1)
-column = random.randint(0,grid_size-1)
-
-
-direction = None
-while direction not in ['n', 's', 'e', 'w']:
-    direction = input("Enter a direction ('n', 's', 'e' or 'w'): ")
-if direction == 'n':
-    row-=1
-    direction_name = "North"
-elif direction == 's':
-    row +=1
-    direction_name = "South"
-elif direction == 'e':
-    column +=1
-    direction_name = "East"
-elif direction == 'w':
-    column -=1
-    direction_name = "West"
-#Clip values
-if column<0:
-    column = 0
-elif column>=grid_size:
-    column = grid_size-1
-if row<0: 
-    row = 0
-elif row>=grid_size:
-    row = grid_size-1
-
+row, column = generate_position()
 age = 4
 id = 1
 
+
 message = f'My name is {name} and my id is {id}.'
-print(message)
-#Find quadrant
-if row<grid_size//2:
-    row_half = "top"
-else:
-    row_half = "bottom"
 
-if column<grid_size//2:
-    column_half = "left"
-else:
-    column_half = "right"
+direction = input_direction()
+stop_point = (grid_size-1, grid_size-1)
+send_message(row, column, direction, grid_size)
 
-quadrant = row_half+" "+column_half
-print(f"Current location: ({row},{column}), {quadrant} quadrant. I am facing {direction_name}")
+
+while (row,column) != stop_point:
+    row, column = move_a_square(row, column, direction)
+    print("Moving one step forward")
+    if is_stuck(row, column, direction):
+        direction = turn_right(direction)
+    send_message(row, column, direction, grid_size)
+
+
+
+
+
+
+
+
 
 
 
