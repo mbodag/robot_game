15ffb88fa1ac568a34dc80945f1d690b50f57acd
Matis Bodaghi <matisbodaghi@gmail.com>
2023-09-22 15:57:00 +0100

    Updated methods and functions and added docstrings

--- main.py
+++ main.py
@@ -1,61 +1,32 @@
 import random
 from robot import Robot
 
+def ask_name():
+    ''' 
+    Prompts the user for the name of the robot and returns it
+    '''
+    return input("What is the robot's name? ")
 
-def run_simulation(robot, grid_size=10, target_row=9, target_column=9):
-    name, id, (row, column), direction = robot.name, robot.id, robot.position, robot.direction
-    print(robot)
-    navigate_robot(row, column, direction, grid_size, target_row, target_column)
-    
-
-def navigate_robot(row, column, direction, grid_size, target_row, target_column):
-    stop_point = (target_row, target_column)
-    send_message(row, column, direction, grid_size)
-
-    while (row,column) != stop_point:
-        row, column = move_a_square(row, column, direction, grid_size)
-        print("Moving one step forward")
-        if is_stuck(row, column, direction, grid_size):
-            direction = turn_right(direction)
-        send_message(row, column, direction, grid_size)
-
-def move_a_square(row, column, direction, grid_size):
-    if direction == 'n':
-        row = max(0, row-1)
-    elif direction == 's':
-        row = min(grid_size-1, row+1)
-    elif direction == 'e':
-        column = min(grid_size-1, column+1)    
-    elif direction == 'w':
-        column = max(0, column-1)
-    
-    return row, column
-
-
-def is_stuck(row, column, direction, grid_size):
-    return (row, column) == move_a_square(row, column, direction, grid_size)
-
-
-def turn_right(direction):
-    list_of_directions = ['n', 'e', 's', 'w']
-    for d in range(4):
-        if list_of_directions[d] == direction:
-            return(list_of_directions[(d+1) % 4])
-    print('Turning right')
-    
 
 def find_direction_name(direction):
+    '''
+    Returns the full name of the direction
+    '''
     return {'n': 'North', 'e': 'East', 's': 'South', 'w': 'West'}[direction]
 
 
-def send_message(row, column, direction, grid_size):
+def send_message(row, column, direction):
+    '''
+    Send the user a message with the current location and direction
+    '''
     direction_name = find_direction_name(direction)
     print(f"Current location: ({row},{column}). I am facing {direction_name}")
 
 
 
 # Randomise robot name and coordinates
-run_simulation(Robot("John"))
+rob1 = Robot(ask_name())
+rob1.navigate()
 
 
 
--- robot.py
+++ robot.py
@@ -13,14 +13,19 @@ class Robot:
         self.target = (9,9)
 
     def __repr__(self):
-        return f"Hi, my name is {self.name}. My ID is {self.id}"
+        return (f"{self.name}, id = {self.id}")
     
     def turn_right(self):
+        '''Changes robot direction to the one 90 degrees clockwise'''
         directions = ['n', 'e', 's', 'w']
         idx = directions.index(self.direction)
         self.direction = directions[(idx+1)%4]
     
     def move_a_square(self):
+        '''
+        Moves robot forward by one square in its current direction
+        If the robot is facing the edge of the board it remains in the same position
+        '''
         row = self.position[0]
         column = self.position[1]
         if self.direction == 'n':
@@ -34,11 +39,18 @@ class Robot:
         self.position = (row, column)
 
     def move_forward_to_wall(self):
+        '''
+        Robot moves in its direction until it reaches a wall
+        '''
         while not self.is_facing_wall():
             self.move_a_square()
 
 
     def is_facing_wall(self):
+        '''
+        Checks if the robot is facing a wall
+        Returns a boolean
+        '''
         facing_north_wall = self.position[0] == 0 and self.direction == 'n'
         facing_south_wall = self.position[0] == self.grid_size-1 and self.direction == 's'
         facing_east_wall = self.position[1] == 0 and self.direction == 'w'
@@ -47,10 +59,16 @@ class Robot:
         return facing_north_wall or facing_south_wall or facing_west_wall or facing_east_wall
 
     def is_at_target(self):
+        '''
+        Returns whether the robot is at its target
+        '''
         return self.position == self.target
 
 
     def navigate(self):
+        '''
+        The robot moves forward until it reaches a wall then turns right, and does so until it reaches its target
+        '''
         counter = 0
         while not self.is_at_target():
             self.move_forward_to_wall()
@@ -58,10 +76,18 @@ class Robot:
             self.turn_right()
             counter +=1
 
+
+    def print_greeting(self):
+        '''
+        Prints a greeting including the robot's name and id
+        '''
+        print(f"Hi, my name is {self.name}. My ID is {self.id}")
+
+
 if __name__ == "__main__":
     jj = Robot('jj')
     jj.navigate()
-    print(jj.position)
+    print(jj)
         
 
         
